2d594d0f4bf0854bd6888bf2405c0ca1bdaf2040
Archana Panda <panda_archana@yahoo.com>
2018-09-26 15:59:17 -0700

    Added the music test

--- IQTestNew/ListenVC.swift
+++ IQTestNew/ListenVC.swift
@@ -11,22 +11,44 @@ import AVFoundation
 
 class ListenVC: UIViewController, AVAudioPlayerDelegate {
     
-    var soundOfMusic:String = ""
+    var soundOfPlay:String = ""
     var audioPlayer:AVAudioPlayer!
+    var checkMusic:Int = 0
+    var score:Int = 0
+    var soundOfMusic:String = ""
+    var total:Int = 0
     
-    let soundArray = ["note1","note2","note3","note4","note5","note6","note1"]
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    let soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
     
     @IBAction func notePressed(_ sender: UIButton) {
-        soundOfMusic = soundArray[sender.tag-1]
-        playMusic()
+        total = total+1
+        if checkMusic == 0{
+            soundOfMusic = soundArray[sender.tag-1]
+            playMusic()
+        } else {
+            soundOfMusic = soundArray[sender.tag-1]
+            if soundOfMusic == soundOfPlay{
+                ProgressHUD.showSuccess("Wow! Correct !")
+                score += 1
+            } else{
+                ProgressHUD.showError("Sorry.. lets try again....")
+            }
+        }
+       updateUI()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    func updateUI(){
+        scoreLabel.text = "Score \(score) out of \(total)"
+    }
     
     func playMusic(){
-        let soundURL = Bundle.main.url(forResource:soundOfNote, withExtension:"wav")!
+        let soundURL = Bundle.main.url(forResource:soundOfMusic, withExtension:"wav")!
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
         } catch let error as NSError{
@@ -34,4 +56,15 @@ class ListenVC: UIViewController, AVAudioPlayerDelegate {
         }
         audioPlayer.play()
     }
+    
+    @IBAction func playPressed(_ sender: UIButton) {
+        var randomMusicNumber = Int(arc4random_uniform(6))
+        soundOfPlay = soundArray[randomMusicNumber]
+        print(soundOfPlay)
+        playMusic()
+        checkMusic = 1
+    }
+    
 }
+
+
