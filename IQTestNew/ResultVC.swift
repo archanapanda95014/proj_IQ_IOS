84673f0180a17802bf0b5ee978d7c64d3f51cd3c
Archana Panda <panda_archana@yahoo.com>
2018-10-24 17:47:37 -0700

    added on oct 24

---
+++ IQTestNew/ResultVC.swift
@@ -0,0 +1,20 @@
+//
+//  ResultVC.swift
+//  IQTestNew
+//
+//  Created by Archana Panda on 9/28/18.
+//  Copyright © 2018 Archana Panda. All rights reserved.
+//
+
+import UIKit
+import DDSpiderChart
+
+class ResultVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
